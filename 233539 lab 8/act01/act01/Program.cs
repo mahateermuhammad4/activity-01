a12d27fbda2567903ea3c59170463a8deae215aa
mahateermuhammad4 <233539@students.au.edu.pk>
2024-11-20 14:37:29 +0500

    Add files via upload

---
+++ 233539 lab 8/act01/act01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.OleDb;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + "C:\\Users\\233539\\Downloads\\Northwind.mdb;User Id=admin;Password=;";
+
+        string queryString = "SELECT ProductID, UnitPrice, ProductName FROM products WHERE UnitPrice > ? ORDER BY UnitPrice DESC;";
+        int paramValue = 5;
+
+        using (OleDbConnection connection = new OleDbConnection(connectionString))
+        {
+            using (OleDbCommand command = new OleDbCommand(queryString, connection))
+            {
+                command.Parameters.AddWithValue("@pricePoint", paramValue);
+
+                try
+                {
+                    connection.Open();
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
